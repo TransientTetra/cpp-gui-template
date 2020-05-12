e3322866bd6d9d58c84af98cbb7dc21ed868a0f1
msperkow <sperkowski.mikolaj@gmail.com>
2020-05-12 18:29:30 +0200

    Change to googletest

--- tst/test.cpp
+++ tst/test.cpp
@@ -1,10 +1,5 @@
-#define BOOST_TEST_MODULE TestSuite
-#define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(TestSuite)
-	BOOST_AUTO_TEST_CASE(test)
-	{
-		BOOST_REQUIRE_EQUAL(1, 1);
-	}
-BOOST_AUTO_TEST_SUITE_END()
+#include "gtest/gtest.h"
+TEST(TestSuite, testCase)
+{
+	ASSERT_EQ(1, 1);
+}
